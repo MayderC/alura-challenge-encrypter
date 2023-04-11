3fa088821f26c3a4b957ce87b69b712bc598ee62
Mayder C <44930667+MayderC@users.noreply.github.com>
2023-04-11 14:44:51 -0600

    feat: add encrypter and desencerypter

---
+++ main.js
@@ -0,0 +1,31 @@
+const vowels = {
+  a: "ai",
+  e: "enter",
+  i: "imes",
+  o: "ober",
+  u: "ufat",
+};
+
+const words = Object.entries(vowels).reduce(
+  (ac, acc) => ({ ...ac, [acc[1]]: acc[0] }),
+  {}
+);
+
+const getRegEncryptedWords = () => {
+  const regx = new RegExp(
+    `${Object.keys(words)
+      .map((x) => `(${x})`)
+      .join("|")}`,
+    "g"
+  );
+  return regx;
+};
+
+const desencrypter = (...match) => words[match[0]];
+const desencrypt = (msg) => msg.replace(getRegEncryptedWords(), desencrypter);
+const encrypter = (...match) => vowels[match[0]];
+const encrypt = (text) => text.replace(/[aeiou]/g, encrypter);
+
+const encrypted = encrypt("gato gato gato");
+console.log(encrypted);
+console.log(desencrypt(encrypted));
