0983fe294ed819723f89a2c5ee2ecb7afcb2f6c9
Mayder C <44930667+MayderC@users.noreply.github.com>
2023-04-17 22:38:09 -0600

    fix: encript logic

--- main.js
+++ main.js
@@ -11,17 +11,32 @@ const words = Object.entries(vowels).reduce(
   {}
 );
 
-const getRegEncryptedWords = () => {
-  const regx = new RegExp(
-    `${Object.keys(words)
-      .map((x) => `(${x})`)
-      .join("|")}`,
-    "g"
-  );
-  return regx;
-};
-
+const getRegEncrypt = () => new RegExp(`${Object.keys(words).join("|")}`, "g");
+const desencrypt = (msg) => msg.replace(getRegEncrypt(), desencrypter);
 const desencrypter = (...match) => words[match[0]];
 const encrypter = (...match) => vowels[match[0]];
-const desencrypt = (msg) => msg.replace(getRegEncryptedWords(), desencrypter);
 const encrypt = (text) => text.replace(/[aeiou]/g, encrypter);
+
+const textArea = document.getElementById("textarea");
+const output = document.getElementById("output");
+const toggle = document.getElementById("toggle");
+const btnClear = document.getElementById("clear");
+let toggleLogic = true;
+toggle.textContent = toggleLogic ? "Encriptando" : "Desencriptando";
+const proxyInputTarget = (target, value) =>
+  (document.getElementById(target).value = value);
+const clearText = (target) => (document.getElementById(target).value = "");
+textArea.addEventListener("input", (e) => {
+  if (toggleLogic) return proxyInputTarget("output", encrypt(e.target.value));
+  proxyInputTarget("output", desencrypt(e.target.value));
+});
+btnClear.addEventListener("click", () => {
+  clearText("textarea");
+  clearText("output");
+});
+toggle.addEventListener("click", () => {
+  toggleLogic = !toggleLogic;
+  const tempValue = output.value;
+  output.value = textArea.value;
+  textArea.value = tempValue;
+});
